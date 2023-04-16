d9741313e5cbfc222a238faa7a1e95c78baa28ff
大陸 永井 <k022g0053@m.neec.ac.jp>
2023-04-16 14:45:38 +0900

    Matrix4x4 の関数を Multiply まで実装

---
+++ Matrix4x4.cpp
@@ -0,0 +1,39 @@
+#include "Matrix4x4.h"
+
+Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 temp = m1;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			temp.m[i][j] += m2.m[i][j];
+		}
+	}
+	return temp;
+}
+
+Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 temp = m1;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			temp.m[i][j] -= m2.m[i][j];
+		}
+	}
+	return temp;
+}
+
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 temp;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			temp.m[i][j] = m1.m[i][0] * m2.m[0][j] +m1.m[i][1] * m2.m[1][j] +m1.m[i][2] * m2.m[2][j] +m1.m[i][3] * m2.m[3][j];
+		}
+	}
+	return temp;
+}
+
+Matrix4x4 Inverse(const Matrix4x4& m1) {
+
+}
+
+Matrix4x4 Transpose(const Matrix4x4& m1) {
+
+}
