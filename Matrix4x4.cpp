567741a4c8110193a10b194777abf2900869be02
大陸 永井 <k022g0053@m.neec.ac.jp>
2023-04-24 10:08:48 +0900

    Mymath ファイルを追加

--- Matrix4x4.cpp
+++ Matrix4x4.cpp
@@ -238,42 +238,42 @@ Matrix4x4 Transpose(const Matrix4x4& m1) {
 	}
 	return temp;
 }
-
-Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rot, const Vector3& translate) {
-
-	Matrix4x4 scaleMatrix_ = {
-		scale.x,0,0,0,
-		0,scale.y,0,0,
-		0,0,scale.z,0,
-		0,0,0,1
-	};
-
-	Matrix4x4 rotZ_ = {
-		std::cosf(rot.z),std::sinf(rot.z),0,0,
-		-std::sinf(rot.z),std::cosf(rot.z),0,0,
-		0,0,1,0,
-		0,0,0,1
-	};
-	Matrix4x4 rotX_ = {
-		1,0,0,0,
-		0,std::cosf(rot.x),std::sinf(rot.x),0,
-		0,-std::sinf(rot.x),std::cosf(rot.x),0,
-		0,0,0,1
-	};
-	Matrix4x4 rotY_ = {
-		std::cosf(rot.y),0,-std::sinf(rot.y),0,
-		0,1,0,0,
-		std::sinf(rot.y),0,std::cosf(rot.y),0,
-		0,0,0,1
-	};
-	Matrix4x4 rotateMatrix_ = Multiply(Multiply(rotX_, rotY_), rotZ_);
-
-	Matrix4x4 translateMatrix_ = {
-		1,0,0,0,
-		0,1,0,0,
-		0,0,1,0,
-		translate.x,translate.y,translate.z,1
-	};
-
-	return Multiply(Multiply(scaleMatrix_, rotateMatrix_), translateMatrix_);
-}
+//
+//Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rot, const Vector3& translate) {
+//
+//	Matrix4x4 scaleMatrix_ = {
+//		scale.x,0,0,0,
+//		0,scale.y,0,0,
+//		0,0,scale.z,0,
+//		0,0,0,1
+//	};
+//
+//	Matrix4x4 rotZ_ = {
+//		std::cosf(rot.z),std::sinf(rot.z),0,0,
+//		-std::sinf(rot.z),std::cosf(rot.z),0,0,
+//		0,0,1,0,
+//		0,0,0,1
+//	};
+//	Matrix4x4 rotX_ = {
+//		1,0,0,0,
+//		0,std::cosf(rot.x),std::sinf(rot.x),0,
+//		0,-std::sinf(rot.x),std::cosf(rot.x),0,
+//		0,0,0,1
+//	};
+//	Matrix4x4 rotY_ = {
+//		std::cosf(rot.y),0,-std::sinf(rot.y),0,
+//		0,1,0,0,
+//		std::sinf(rot.y),0,std::cosf(rot.y),0,
+//		0,0,0,1
+//	};
+//	Matrix4x4 rotateMatrix_ = Multiply(Multiply(rotX_, rotY_), rotZ_);
+//
+//	Matrix4x4 translateMatrix_ = {
+//		1,0,0,0,
+//		0,1,0,0,
+//		0,0,1,0,
+//		translate.x,translate.y,translate.z,1
+//	};
+//
+//	return Multiply(Multiply(scaleMatrix_, rotateMatrix_), translateMatrix_);
+//}
---
+++ Mymath.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//struct Matrix3x3 {
+//	float m[3][3];
+//};
+
+struct Matrix4x4;
+
+struct Vector2;
+struct Vector3;
+struct Vector4;
+
+//Matrix3x3 MakeAffineMatrix(const Vector2& scale, const Vector2& rotate, const Vector2& translate);
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
