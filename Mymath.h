34ee4e682e9708e8d1b5203055c5866cdc00608a
大陸 永井 <k022g0053@m.neec.ac.jp>
2023-04-24 12:16:57 +0900

    ネームスペースにしてみた

--- Mymath.h
+++ Mymath.h
@@ -4,11 +4,16 @@
 //	float m[3][3];
 //};
 
-struct Matrix4x4;
+namespace Mymath
+{
 
-struct Vector2;
-struct Vector3;
-struct Vector4;
+	struct Matrix4x4;
 
-//Matrix3x3 MakeAffineMatrix(const Vector2& scale, const Vector2& rotate, const Vector2& translate);
-Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+	struct Vector2;
+	struct Vector3;
+	struct Vector4;
+
+	//Matrix3x3 MakeAffineMatrix(const Vector2& scale, const Vector2& rotate, const Vector2& translate);
+	Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+
+}
