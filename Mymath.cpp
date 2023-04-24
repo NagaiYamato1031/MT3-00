7617ac8eb0106191d3ab3698aa95b5487cdd26ea
大陸 永井 <k022g0053@m.neec.ac.jp>
2023-04-24 14:07:37 +0900

    Mymath に大体まとめた

--- Mymath.cpp
+++ Mymath.cpp
@@ -1,14 +1,310 @@
 #include "Mymath.h"
 
-#include "Matrix4x4.h"
 #include "Vector2.h"
 #include "Vector3.h"
 #include "Vector4.h"
 
+#include "Matrix4x4.h"
+
 #include <cmath>
 
+//using namespace Mymath;
 
-Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rot, const Vector3& translate) {
+#pragma region Vector
+
+#pragma region Vector2
+
+
+
+// End Vector2
+#pragma endregion
+
+#pragma region Vector3
+
+// â¡éZ
+Vector3 Mymath::Add(const Vector3& v1, const Vector3& v2) {
+	return Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+}
+
+// å∏éZ
+Vector3 Mymath::Subtract(const Vector3& v1, const Vector3& v2) {
+	return Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+}
+
+// ÉXÉJÉâÅ[î{
+Vector3 Mymath::Multiply(float scalar, const Vector3& v) {
+	return Vector3(scalar * v.x, scalar * v.y, scalar * v.z);
+}
+
+// ì‡êœ
+float Mymath::Dot(const Vector3& v1, const Vector3& v2) {
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+// í∑Ç≥
+float Mymath::Length(const Vector3& v) {
+	return sqrtf(Dot(v, v));
+}
+
+// ê≥ãKâª
+Vector3 Mymath::Normalize(const Vector3& v) {
+	float length = Length(v);
+	Vector3 temp = v;
+	if (length != 0) {
+		temp.x /= length;
+		temp.y /= length;
+		temp.z /= length;
+	}
+	return temp;
+}
+// End Vector3
+#pragma endregion
+
+#pragma region Vector4
+
+
+
+// End Vector4
+#pragma endregion
+
+
+// End Vector
+#pragma endregion
+
+#pragma region Matrix
+
+#pragma region Matrix4x4
+
+Matrix4x4 Mymath::Add(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 temp = m1;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			temp.m[i][j] += m2.m[i][j];
+		}
+	}
+	return temp;
+}
+
+Matrix4x4 Mymath::Subtract(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 temp = m1;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			temp.m[i][j] -= m2.m[i][j];
+		}
+	}
+	return temp;
+}
+
+Matrix4x4 Mymath::Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 temp;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			temp.m[i][j] = m1.m[i][0] * m2.m[0][j] + m1.m[i][1] * m2.m[1][j] + m1.m[i][2] * m2.m[2][j] + m1.m[i][3] * m2.m[3][j];
+		}
+	}
+	return temp;
+}
+
+Matrix4x4 Mymath::Inverse(const Matrix4x4& matrix) {
+	float det;
+	det = matrix.m[0][0] * matrix.m[1][1] * matrix.m[2][2] * matrix.m[3][3]
+		+ matrix.m[0][0] * matrix.m[1][2] * matrix.m[2][3] * matrix.m[3][1]
+		+ matrix.m[0][0] * matrix.m[1][3] * matrix.m[2][1] * matrix.m[3][2]
+
+		- matrix.m[0][0] * matrix.m[1][3] * matrix.m[2][2] * matrix.m[3][1]
+		- matrix.m[0][0] * matrix.m[1][2] * matrix.m[2][1] * matrix.m[3][3]
+		- matrix.m[0][0] * matrix.m[1][1] * matrix.m[2][3] * matrix.m[3][2]
+
+		- matrix.m[0][1] * matrix.m[1][0] * matrix.m[2][2] * matrix.m[3][3]
+		- matrix.m[0][2] * matrix.m[1][0] * matrix.m[2][3] * matrix.m[3][1]
+		- matrix.m[0][3] * matrix.m[1][0] * matrix.m[2][1] * matrix.m[3][2]
+
+		+ matrix.m[0][3] * matrix.m[1][0] * matrix.m[2][2] * matrix.m[3][1]
+		+ matrix.m[0][2] * matrix.m[1][0] * matrix.m[2][1] * matrix.m[3][3]
+		+ matrix.m[0][1] * matrix.m[1][0] * matrix.m[2][3] * matrix.m[3][2]
+
+		+ matrix.m[0][1] * matrix.m[1][2] * matrix.m[2][0] * matrix.m[3][3]
+		+ matrix.m[0][2] * matrix.m[1][3] * matrix.m[2][0] * matrix.m[3][1]
+		+ matrix.m[0][3] * matrix.m[1][1] * matrix.m[2][0] * matrix.m[3][2]
+
+		- matrix.m[0][3] * matrix.m[1][2] * matrix.m[2][0] * matrix.m[3][1]
+		- matrix.m[0][2] * matrix.m[1][1] * matrix.m[2][0] * matrix.m[3][3]
+		- matrix.m[0][1] * matrix.m[1][3] * matrix.m[2][0] * matrix.m[3][2]
+
+		- matrix.m[0][1] * matrix.m[1][2] * matrix.m[2][3] * matrix.m[3][0]
+		- matrix.m[0][2] * matrix.m[1][3] * matrix.m[2][1] * matrix.m[3][0]
+		- matrix.m[0][3] * matrix.m[1][1] * matrix.m[2][2] * matrix.m[3][0]
+
+		+ matrix.m[0][3] * matrix.m[1][2] * matrix.m[2][1] * matrix.m[3][0]
+		+ matrix.m[0][2] * matrix.m[1][1] * matrix.m[2][3] * matrix.m[3][0]
+		+ matrix.m[0][1] * matrix.m[1][3] * matrix.m[2][2] * matrix.m[3][0];
+
+	float det11 = 1 / det * (
+		+matrix.m[1][1] * matrix.m[2][2] * matrix.m[3][3]
+		+ matrix.m[1][2] * matrix.m[2][3] * matrix.m[3][1]
+		+ matrix.m[1][3] * matrix.m[2][1] * matrix.m[3][2]
+		- matrix.m[1][3] * matrix.m[2][2] * matrix.m[3][1]
+		- matrix.m[1][2] * matrix.m[2][1] * matrix.m[3][3]
+		- matrix.m[1][1] * matrix.m[2][3] * matrix.m[3][2]
+		);
+	float det12 = 1 / det * (
+		-matrix.m[0][1] * matrix.m[2][2] * matrix.m[3][3]
+		- matrix.m[0][2] * matrix.m[2][3] * matrix.m[3][1]
+		- matrix.m[0][3] * matrix.m[2][1] * matrix.m[3][2]
+		+ matrix.m[0][3] * matrix.m[2][2] * matrix.m[3][1]
+		+ matrix.m[0][2] * matrix.m[2][1] * matrix.m[3][3]
+		+ matrix.m[0][1] * matrix.m[2][3] * matrix.m[3][2]
+		);
+	float det13 = 1 / det * (
+		+matrix.m[0][1] * matrix.m[1][2] * matrix.m[3][3]
+		+ matrix.m[0][2] * matrix.m[1][3] * matrix.m[3][1]
+		+ matrix.m[0][3] * matrix.m[1][1] * matrix.m[3][2]
+		- matrix.m[0][3] * matrix.m[1][2] * matrix.m[3][1]
+		- matrix.m[0][2] * matrix.m[1][1] * matrix.m[3][3]
+		- matrix.m[0][1] * matrix.m[1][3] * matrix.m[3][2]
+		);
+	float det14 = 1 / det * (
+		-matrix.m[0][1] * matrix.m[1][2] * matrix.m[2][3]
+		- matrix.m[0][2] * matrix.m[1][3] * matrix.m[2][1]
+		- matrix.m[0][3] * matrix.m[1][1] * matrix.m[2][2]
+		+ matrix.m[0][3] * matrix.m[1][2] * matrix.m[2][1]
+		+ matrix.m[0][2] * matrix.m[1][1] * matrix.m[2][3]
+		+ matrix.m[0][1] * matrix.m[1][3] * matrix.m[2][2]
+		);
+
+	float det21 = 1 / det * (
+		-matrix.m[1][0] * matrix.m[2][2] * matrix.m[3][3]
+		- matrix.m[1][2] * matrix.m[2][3] * matrix.m[3][0]
+		- matrix.m[1][3] * matrix.m[2][0] * matrix.m[3][2]
+		+ matrix.m[1][3] * matrix.m[2][2] * matrix.m[3][0]
+		+ matrix.m[1][2] * matrix.m[2][0] * matrix.m[3][3]
+		+ matrix.m[1][0] * matrix.m[2][3] * matrix.m[3][2]
+		);
+
+	float det22 = 1 / det * (
+		+matrix.m[0][0] * matrix.m[2][2] * matrix.m[3][3]
+		+ matrix.m[0][2] * matrix.m[2][3] * matrix.m[3][0]
+		+ matrix.m[0][3] * matrix.m[2][0] * matrix.m[3][2]
+		- matrix.m[0][3] * matrix.m[2][2] * matrix.m[3][0]
+		- matrix.m[0][2] * matrix.m[2][0] * matrix.m[3][3]
+		- matrix.m[0][0] * matrix.m[2][3] * matrix.m[3][2]
+		);
+
+
+	float det23 = 1 / det * (
+		-matrix.m[0][0] * matrix.m[1][2] * matrix.m[3][3]
+		- matrix.m[0][2] * matrix.m[1][3] * matrix.m[3][0]
+		- matrix.m[0][3] * matrix.m[1][0] * matrix.m[3][2]
+		+ matrix.m[0][3] * matrix.m[1][2] * matrix.m[3][0]
+		+ matrix.m[0][2] * matrix.m[1][0] * matrix.m[3][3]
+		+ matrix.m[0][0] * matrix.m[1][3] * matrix.m[3][2]
+		);
+
+	float det24 = 1 / det * (
+		+matrix.m[0][0] * matrix.m[1][2] * matrix.m[2][3]
+		+ matrix.m[0][2] * matrix.m[1][3] * matrix.m[2][0]
+		+ matrix.m[0][3] * matrix.m[1][0] * matrix.m[2][2]
+		- matrix.m[0][3] * matrix.m[1][2] * matrix.m[2][0]
+		- matrix.m[0][2] * matrix.m[1][0] * matrix.m[2][3]
+		- matrix.m[0][0] * matrix.m[1][3] * matrix.m[2][2]
+		);
+
+	float det31 = 1 / det * (
+		+matrix.m[1][0] * matrix.m[2][1] * matrix.m[3][3]
+		+ matrix.m[1][1] * matrix.m[2][3] * matrix.m[3][0]
+		+ matrix.m[1][3] * matrix.m[2][0] * matrix.m[3][1]
+		- matrix.m[1][3] * matrix.m[2][1] * matrix.m[3][0]
+		- matrix.m[1][1] * matrix.m[2][0] * matrix.m[3][3]
+		- matrix.m[1][0] * matrix.m[2][3] * matrix.m[3][1]
+		);
+
+	float det32 = 1 / det * (
+		-matrix.m[0][0] * matrix.m[2][1] * matrix.m[3][3]
+		- matrix.m[0][1] * matrix.m[2][3] * matrix.m[3][0]
+		- matrix.m[0][3] * matrix.m[2][0] * matrix.m[3][1]
+		+ matrix.m[0][3] * matrix.m[2][1] * matrix.m[3][0]
+		+ matrix.m[0][1] * matrix.m[2][0] * matrix.m[3][3]
+		+ matrix.m[0][0] * matrix.m[2][3] * matrix.m[3][1]
+		);
+
+	float det33 = 1 / det * (
+		+matrix.m[0][0] * matrix.m[1][1] * matrix.m[3][3]
+		+ matrix.m[0][1] * matrix.m[1][3] * matrix.m[3][0]
+		+ matrix.m[0][3] * matrix.m[1][0] * matrix.m[3][1]
+		- matrix.m[0][3] * matrix.m[1][1] * matrix.m[3][0]
+		- matrix.m[0][1] * matrix.m[1][0] * matrix.m[3][3]
+		- matrix.m[0][0] * matrix.m[1][3] * matrix.m[3][1]
+		);
+
+	float det34 = 1 / det * (
+		-matrix.m[0][0] * matrix.m[1][1] * matrix.m[2][3]
+		- matrix.m[0][1] * matrix.m[1][3] * matrix.m[2][0]
+		- matrix.m[0][3] * matrix.m[1][0] * matrix.m[2][1]
+		+ matrix.m[0][3] * matrix.m[1][1] * matrix.m[2][0]
+		+ matrix.m[0][1] * matrix.m[1][0] * matrix.m[2][3]
+		+ matrix.m[0][0] * matrix.m[1][3] * matrix.m[2][1]
+		);
+
+	float det41 = 1 / det * (
+		-matrix.m[1][0] * matrix.m[2][1] * matrix.m[3][2]
+		- matrix.m[1][1] * matrix.m[2][2] * matrix.m[3][0]
+		- matrix.m[1][2] * matrix.m[2][0] * matrix.m[3][1]
+		+ matrix.m[1][2] * matrix.m[2][1] * matrix.m[3][0]
+		+ matrix.m[1][1] * matrix.m[2][0] * matrix.m[3][2]
+		+ matrix.m[1][0] * matrix.m[2][2] * matrix.m[3][1]
+		);
+
+	float det42 = 1 / det * (
+		+matrix.m[0][0] * matrix.m[2][1] * matrix.m[3][2]
+		+ matrix.m[0][1] * matrix.m[2][2] * matrix.m[3][0]
+		+ matrix.m[0][2] * matrix.m[2][0] * matrix.m[3][1]
+		- matrix.m[0][2] * matrix.m[2][1] * matrix.m[3][0]
+		- matrix.m[0][1] * matrix.m[2][0] * matrix.m[3][2]
+		- matrix.m[0][0] * matrix.m[2][2] * matrix.m[3][1]
+		);
+
+	float det43 = 1 / det * (
+		-matrix.m[0][0] * matrix.m[1][1] * matrix.m[3][2]
+		- matrix.m[0][1] * matrix.m[1][2] * matrix.m[3][0]
+		- matrix.m[0][2] * matrix.m[1][0] * matrix.m[3][1]
+		+ matrix.m[0][2] * matrix.m[1][1] * matrix.m[3][0]
+		+ matrix.m[0][1] * matrix.m[1][0] * matrix.m[3][2]
+		+ matrix.m[0][0] * matrix.m[1][2] * matrix.m[3][1]
+		);
+
+	float det44 = 1 / det * (
+		+matrix.m[0][0] * matrix.m[1][1] * matrix.m[2][2]
+		+ matrix.m[0][1] * matrix.m[1][2] * matrix.m[2][0]
+		+ matrix.m[0][2] * matrix.m[1][0] * matrix.m[2][1]
+		- matrix.m[0][2] * matrix.m[1][1] * matrix.m[2][0]
+		- matrix.m[0][1] * matrix.m[1][0] * matrix.m[2][2]
+		- matrix.m[0][0] * matrix.m[1][2] * matrix.m[2][1]
+		);
+
+
+	Matrix4x4 result = {
+		det11, det12, det13, det14,
+		det21, det22, det23, det24,
+		det31, det32, det33, det34,
+		det41, det42, det43, det44 };
+	return result;
+}
+
+Matrix4x4 Mymath::Transpose(const Matrix4x4& matrix) {
+	Matrix4x4 temp;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			temp.m[i][j] = matrix.m[j][i];
+		}
+	}
+	return temp;
+}
+
+Matrix4x4 Mymath::MakeIdentity4x4() {
+	return Matrix4x4{ 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
+}
+
+Matrix4x4 Mymath::MakeAffineMatrix4x4(const Vector3& scale, const Vector3& rot, const Vector3& translate) {
 
 	Matrix4x4 scaleMatrix_ = {
 		scale.x,0,0,0,
@@ -46,3 +342,8 @@ Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rot, const Vecto
 
 	return Multiply(Multiply(scaleMatrix_, rotateMatrix_), translateMatrix_);
 }
+// End Matrix4x4
+#pragma endregion
+
+// End Matrix
+#pragma endregion
